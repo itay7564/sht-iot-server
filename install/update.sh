8cf227c270dd9390d48ef35ca05e34adc22502a9
itay7564 <22itaysh@gmail.com>
2021-03-16 22:41:27 +0200

    fix read once again

--- install/update.sh
+++ install/update.sh
@@ -8,7 +8,7 @@ fi
 
 echo "You can only run this script after installing sht-iot successfuly. run install.sh to do so."
 echo "Also, verify that this clone of  https://github.com/itay7564/sht-iot-server/ is up-to-date"
-read REPLY -p "To continue press enter, to exit press Ctrl+C"
+read -p "To continue press enter, to exit press Ctrl+C" REPLY 
 
 
 cp sht-iot.sh /opt/sht-iot/
